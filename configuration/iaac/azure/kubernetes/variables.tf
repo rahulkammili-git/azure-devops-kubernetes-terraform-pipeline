3a812e0da7e707f8aaf4f336bc68fc07f1a7c599
rahulkammili-git <kammilirahul@outlook.com>
2021-01-05 13:48:22 +0530

    Update variables.tf

--- configuration/iaac/azure/kubernetes/variables.tf
+++ configuration/iaac/azure/kubernetes/variables.tf
@@ -1,7 +1,3 @@
-variable client_id {}
-variable client_secret {}
-variable ssh_public_key {}
-
 variable environment {
     default = "dev"
 }
@@ -13,8 +9,6 @@ variable location {
 variable node_count {
   default = 2
 }
-
-
 
 variable dns_prefix {
   default = "k8stest"
